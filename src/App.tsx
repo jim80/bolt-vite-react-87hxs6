c825cddf575bebeb9e79a2e5bc970f01806555c9
jim80 <hayes.jim@gmail.com>
2024-10-13 00:23:30 +0100

    make background very dark grey

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import DrawingList from './components/DrawingList';
 import DrawingCard from './components/DrawingCard';
 import SearchForm from './components/SearchForm';
